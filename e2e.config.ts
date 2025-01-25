439ce516558137e692d272755d7403b394ba8f07
Thomas <przegralek.thomas@gmail.com>
2025-01-25 21:06:01 +0100

    Add e2e.config.ts for end-to-end test configuration

---
+++ e2e.config.ts
@@ -0,0 +1,31 @@
+import { PlaywrightTestConfig } from "@playwright/test";
+
+const config: PlaywrightTestConfig = {
+    timeout: 50000,
+    retries: 0, //retry failing test
+    testDir: 'tests/e2e',
+    reporter: [['list']],
+    use: {
+        headless: false, 
+        actionTimeout: 10000,
+        ignoreHTTPSErrors: true,
+        video: "off", 
+        screenshot: "off",
+    },
+    projects: [
+        {
+          name: 'Chromium',
+          use: { browserName: 'chromium' },
+        },
+        {
+          name: 'Firefox',
+          use: { browserName: 'firefox' },
+        },
+        {
+          name: 'Webkit',
+          use: { headless: false, browserName: 'webkit' },
+        },
+      ],
+    };
+     
+export default config;
