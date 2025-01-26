685365aaa88d9c0d30e34711fa761eca985870b4
Thomas <przegralek.thomas@gmail.com>
2025-01-26 18:26:59 +0100

    Modifications sur e2e-foreignCurrency.spec.ts

--- tests/e2e/e2e-foreignCurrency.spec.ts
+++ tests/e2e/e2e-foreignCurrency.spec.ts
@@ -1,44 +1,38 @@
 import {test, expect} from '@playwright/test'
 
-test.describe('Foreign Currency Flow', async () => {
-    test.beforeEach(async ({page}) => {
-        await page.goto('http://zero.webappsecurity.com/index.html')
-        await page.getByRole('button', {name:'Signin'}).click()
-        await page.locator('#user_login').fill('username')
-        await page.locator('#user_password').fill('password')
-        await page.locator('text= Sign In').click()
-        await page.goto('http://zero.webappsecurity.com/index.html')
-    })
-
-    test('Foreign Currency', async ({page}) => {
-        await page.locator('#onlineBankingMenu').click()
-        await page.locator('#pay_bills_link').click()
-        await page.locator('text=Purchase Foreign Currency').click()
-
-        await page.selectOption('#pc_currency', 'EUR')
-        const conversion = await page.locator("#sp_sell_rate")
-        await expect(conversion).toBeVisible()
-        await expect(conversion).toContainText('euro')
-
-        await page.fill('#pc_amount', '500')
-        await page.getByRole('button', {name:"Calculate Costs"}).click()
-        page.on('dialog', async dialog => {
-            expect(dialog.message()).toBe('Please, ensure that you have filled all the required fields with valid values.');
-            await dialog.accept()
-        });
-        await page.locator('#pc_inDollars_true').check()
-        await page.getByRole('button', {name:"Calculate Costs"}).click()
-
-        const conversionFinal = await page.locator('#pc_conversion_amount')
-        await expect(conversionFinal).toBeVisible()
-    })
-})
-
-
-
-
-
-
-
-
+test.describe('Currency Exchange Form', () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto('http://zero.webappsecurity.com/index.html');
+      await page.click('#signin_button');
+      await page.fill('#user_login', 'username');
+      await page.fill('#user_password', 'password');
+      await page.click('text=Sign in');
+      await page.goto('http://zero.webappsecurity.com/index.html')
+    });
+  
+    test('Should make currency exchange', async ({ page }) => {
+      await page.locator('#onlineBankingMenu').click()
+      await page.locator('#pay_bills_link').click()
+      await page.click('#pay_bills_tab');
+      await page.click('text=Purchase Foreign Currency');
+      await page.selectOption('#pc_currency', 'EUR');
+  
+      const rate = await page.locator('#sp_sell_rate');
+      await expect(rate).toContainText('1 euro (EUR)');
+  
+      await page.fill('#pc_amount', '1000');
+      await page.click('#pc_inDollars_true');
+      await page.click('#pc_calculate_costs');
+  
+      const conversionAmount = await page.locator('#pc_conversion_amount');
+      await expect(conversionAmount).toContainText('1000.00 U.S. dollar (USD)');
+  
+      await page.click('#purchase_cash');
+  
+      const message = await page.locator('#alert_content');
+      await expect(message).toBeVisible();
+      await expect(message).toContainText('Foreign currency cash was successfully purchased');
+    });
+  });
+  
 
