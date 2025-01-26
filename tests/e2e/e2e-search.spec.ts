43c5674e0823742ec29c3d40d5772abc7007bc5b
Thomas <przegralek.thomas@gmail.com>
2025-01-26 18:27:27 +0100

    Modifications sur e2e-search.spec.ts

--- tests/e2e/e2e-search.spec.ts
+++ tests/e2e/e2e-search.spec.ts
@@ -1,11 +1,13 @@
 import { test, expect } from '@playwright/test';
+import { HomePage } from '../../pages-objects/HomePage';
 
 test.describe('Search Results', () => {
-  test('Should find search results', async ({ page }) => {
-    await page.goto('http://zero.webappsecurity.com/index.html');
-    await page.locator('#searchTerm').fill('bank')
-    await page.keyboard.press('Enter');
+  let homePage : HomePage
 
+  test('Should find search results', async ({page}) => {
+    homePage = new HomePage(page)
+    await homePage.visit()
+    await homePage.searchFor('bank')
     const numberOfLinks = await page.locator('li > a');
     await expect(numberOfLinks).toHaveCount(2);
   });
