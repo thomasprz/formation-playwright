dde77fd1a7872730dec3eea0c53c87e9c05a656d
Thomas <przegralek.thomas@gmail.com>
2025-01-26 18:27:17 +0100

    Modifications sur e2e-login.spec.ts

--- tests/e2e/e2e-login.spec.ts
+++ tests/e2e/e2e-login.spec.ts
@@ -1,32 +1,27 @@
 import {test,expect} from '@playwright/test'
+import { LoginPage } from '../../pages-objects/loginPage'
+import { HomePage } from '../../pages-objects/HomePage' // Importer la page object
 
 test.describe.parallel("Login/Logout Flow", () => {
+    let loginPage: LoginPage // Création d'une variable pour la page object
+    let homePage : HomePage
 
-test.beforeEach(async ({ page }) => {
-    await page.goto('http://zero.webappsecurity.com/index.html')
-  })
+    test.beforeEach(async ({ page }) => {
+        loginPage = new LoginPage(page) // Initialisation ( de préférence le positionner dans before Hook car il sera accessible dans tous les tests en dessous)
+        homePage = new HomePage(page)
+        await homePage.visit()
+    })
 
-  test('Negative Scenario for login', async ({ page }) => {
-        await page.click('#signin_button')
-        await page.fill('#user_login', 'invalid username')
-        await page.fill('#user_password', 'invalid password')
-        await page.click('text=Sign in')
-    
-        const errorMessage = await page.locator('.alert-error')
-        await expect(errorMessage).toContainText('Login and/or password are wrong')
+    test('Negative Scenario for login', async ({ page }) => {
+        await homePage.clickOnSignIn()
+        await loginPage.login('invalid username', 'invalid password')
+        await loginPage.assertErrorMessage()
     })
     
-    test('Positive Scenario for login + logout', async ({ page }) => {
-        await page.click('#signin_button')
-        await page.fill('#user_login', 'username')
-        await page.fill('#user_password', 'password')
-        await page.click('text=Sign in')
+    test('Positive Scenario for login', async ({ page }) => {
+        await homePage.clickOnSignIn()
+        await loginPage.login('username', 'password')
         await page.goto('http://zero.webappsecurity.com/bank/transfer-funds.html')
-
-        const accountSummaryTab = await page.locator('#account_summary_tab')
-        await expect(accountSummaryTab).toBeVisible()
-
-        await page.goto('http://zero.webappsecurity.com/logout.html')
-        await expect(page).toHaveURL('http://zero.webappsecurity.com/index.html')
     })
+
 })
