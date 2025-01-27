ad5a74d49c1854e515989ffe349911812ef61354
Thomas <przegralek.thomas@gmail.com>
2025-01-27 21:52:59 +0100

    Refactorisation des tests de paiement : ajout de méthodes et assertions pour une meilleure couverture des cas d'utilisation

--- tests/e2e/e2e-payment.spec.ts
+++ tests/e2e/e2e-payment.spec.ts
@@ -1,27 +1,32 @@
-import { test, expect } from '@playwright/test'
+import {test, expect} from '@playwright/test'
+import {HomePage} from '../../pages-objects/HomePage';
+import {LoginPage} from '../../pages-objects/loginPage';
+import {PaymentPage} from '../../pages-objects/PaymentPage';
+import {Navbar} from '../../pages-objects/components/Navbar';
 
 test.describe('New Payment', () => {
+    let homepage: HomePage
+    let loginpage: LoginPage
+    let paymentPage: PaymentPage
+    let navbar: Navbar
+
+
   test.beforeEach(async ({ page }) => {
-    await page.goto('http://zero.webappsecurity.com/index.html')
-    await page.click('#signin_button')
-    await page.type('#user_login', 'username')
-    await page.type('#user_password', 'password')
-    await page.click('text=Sign in')
+      homepage = new HomePage(page)
+      loginpage = new LoginPage(page)
+      paymentPage = new PaymentPage(page)
+      navbar = new Navbar(page)
+
+      await homepage.visit()
+      await homepage.clickOnSignIn()
+      await loginpage.login('username','password')
+      await page.goto('http://zero.webappsecurity.com/bank/transfer-funds.html')
   })
 
-  test('Should send new payment', async ({ page }) => {
-    await page.click('#pay_bills_tab')
-    await page.selectOption('#sp_payee', 'apple')
-    await page.click('#sp_get_payee_details')
-    await page.waitForSelector('#sp_payee_details')
-    await page.selectOption('#sp_account', '6')
-    await page.fill('#sp_amount', '5000')
-    await page.fill('#sp_date', '2021-11-09')
-    await page.fill('#sp_description', 'some random message')
-    await page.click('#pay_saved_payees')
-
-    const message = await page.locator('#alert_content > span')
-    await expect(message).toBeVisible()
-    await expect(message).toContainText('The payment was successfully submitted')
+      test('Should send new payment', async ({ page }) => {
+        await navbar.clickOnTab("Pay Bills")
+        await paymentPage.createPayment("apple","6","5000","2021-11-09","some random message")
+        await expect (paymentPage.message).toBeVisible()
+        await expect (paymentPage.message).toContainText('The payment was successfully submitted')
   })
 })
