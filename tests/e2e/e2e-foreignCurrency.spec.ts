0819d77b340aa6360b3b2e0f8436ee3ae9f19926
Thomas <przegralek.thomas@gmail.com>
2025-01-27 21:52:29 +0100

    Ajout de tests pour les transactions en devises étrangères : nouvelles assertions et refactorisation du code

--- tests/e2e/e2e-foreignCurrency.spec.ts
+++ tests/e2e/e2e-foreignCurrency.spec.ts
@@ -1,37 +1,33 @@
 import {test, expect} from '@playwright/test'
+import { HomePage } from '../../pages-objects/HomePage';
+import { LoginPage } from '../../pages-objects/loginPage';
+import { Navbar } from '../../pages-objects/components/Navbar';
+import { PaymentPage } from '../../pages-objects/PaymentPage';
+
 
 test.describe('Currency Exchange Form', () => {
+  let homepage: HomePage
+  let loginpage: LoginPage
+  let paymentpage : PaymentPage
+  let navbar : Navbar
+
     test.beforeEach(async ({ page }) => {
-      await page.goto('http://zero.webappsecurity.com/index.html');
-      await page.click('#signin_button');
-      await page.fill('#user_login', 'username');
-      await page.fill('#user_password', 'password');
-      await page.click('text=Sign in');
-      await page.goto('http://zero.webappsecurity.com/index.html')
+      homepage = new HomePage(page)
+      loginpage = new LoginPage(page)
+      navbar = new Navbar(page)
+      paymentpage = new PaymentPage(page)
+
+      await homepage.visit()
+      await homepage.clickOnSignIn()
+      await loginpage.login('username','password')
+      await page.goto('http://zero.webappsecurity.com/bank/transfer-funds.html')
     });
   
-    test('Should make currency exchange', async ({ page }) => {
-      await page.locator('#onlineBankingMenu').click()
-      await page.locator('#pay_bills_link').click()
-      await page.click('#pay_bills_tab');
-      await page.click('text=Purchase Foreign Currency');
-      await page.selectOption('#pc_currency', 'EUR');
-  
-      const rate = await page.locator('#sp_sell_rate');
-      await expect(rate).toContainText('1 euro (EUR)');
-  
-      await page.fill('#pc_amount', '1000');
-      await page.click('#pc_inDollars_true');
-      await page.click('#pc_calculate_costs');
-  
-      const conversionAmount = await page.locator('#pc_conversion_amount');
-      await expect(conversionAmount).toContainText('1000.00 U.S. dollar (USD)');
-  
-      await page.click('#purchase_cash');
-  
-      const message = await page.locator('#alert_content');
-      await expect(message).toBeVisible();
-      await expect(message).toContainText('Foreign currency cash was successfully purchased');
+    test('Should make currency exchange', async ({page}) => {
+      await navbar.clickOnTab("Pay Bills")
+      await paymentpage.purchaseForeignCurrency("EUR", "5000")
+      await expect(paymentpage.messageSuccess).toBeVisible();
+      await expect(paymentpage.messageSuccess).toContainText('Foreign currency cash was successfully purchased');
     });
   });
   
