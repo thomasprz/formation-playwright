934e5350aee6050122ad8e71a6c04c52ee46fb81
Thomas <przegralek.thomas@gmail.com>
2025-01-27 21:51:57 +0100

    Refactorisation de la LoginPage

--- pages-objects/LoginPage.ts
+++ pages-objects/LoginPage.ts
@@ -1,8 +1,9 @@
 import {expect, Locator, Page} from '@playwright/test'
+import { AbstractPage} from './AbstractPage'
 
-export class LoginPage {
+export class LoginPage extends AbstractPage {
     //Define selectors
-    readonly page : Page 
+    // readonly page : Page 
     readonly signinButton : Locator
     readonly usernameInput: Locator
     readonly passwordInput: Locator
@@ -12,7 +13,8 @@ export class LoginPage {
 
     // Init selectors using constructor
     constructor(page: Page) {
-        this.page = page
+        // this.page = page
+        super(page)
         this.usernameInput = page.locator('#user_login')
         this.passwordInput = page.locator('#user_password')
         this.submitButton = page.locator('text=Sign in')
