a7b345a3ae3690feeda5aaf5d1c8737382ac7544
Thomas <przegralek.thomas@gmail.com>
2025-01-27 21:53:22 +0100

    Refactorisation des tests de transfert de fonds : ajout de nouvelles méthodes et amélioration des assertions

--- tests/e2e/e2e-transfer-funds.spec.ts
+++ tests/e2e/e2e-transfer-funds.spec.ts
@@ -1,29 +1,31 @@
-import { test, expect } from '@playwright/test';
+import {test, expect} from '@playwright/test'
+import {HomePage} from '../../pages-objects/HomePage';
+import {LoginPage} from '../../pages-objects/loginPage';
+import {Navbar} from '../../pages-objects/components/Navbar';
+import {TransferfundsPage} from '../../pages-objects/TransferfundsPage';
 
 test.describe('Transfer Funds and Make Payments', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('http://zero.webappsecurity.com/index.html');
-    await page.click('#signin_button');
-    await page.fill('#user_login', 'username');
-    await page.fill('#user_password', 'password');
-    await page.click('text=Sign in');
-    await page.goto('http://zero.webappsecurity.com/bank/transfer-funds.html')
+  let homepage: HomePage
+  let loginpage: LoginPage
+  let transferfundspage : TransferfundsPage
+  let navbar : Navbar
 
-  });
+    test.beforeEach(async ({ page }) => {
+      homepage = new HomePage(page)
+      loginpage = new LoginPage(page)
+      navbar = new Navbar(page)
+      transferfundspage = new TransferfundsPage(page)
+
+      await homepage.visit()
+      await homepage.clickOnSignIn()
+      await loginpage.login('username','password')
+      await page.goto('http://zero.webappsecurity.com/bank/transfer-funds.html')
+    });
 
   test('Transfer funds', async ({ page }) => {
-    await page.click('#transfer_funds_tab');
-    await page.selectOption('#tf_fromAccountId', '2');
-    await page.selectOption('#tf_toAccountId', '3');
-    await page.fill('#tf_amount', '500');
-    await page.fill('#tf_description', 'Test message');
-    await page.click('#btn_submit');
-
-    const boardHeader = await page.locator('h2.board-header');
-    await expect(boardHeader).toContainText('Verify');
-    await page.click('#btn_submit');
-
-    const message = await page.locator('.alert-success');
-    await expect(message).toContainText('You successfully submitted your transaction');
+    await navbar.clickOnTab("Transfer Funds")
+    await transferfundspage.makeTransferMoney("2", "3", "500", "Test Message")
+    await expect(transferfundspage.successMessage).toContainText('You successfully submitted your transaction');
   });
+
 });
