6a401205e42288b91a9ced51ae88a3af522a6d4f
Thomas <przegralek.thomas@gmail.com>
2025-01-27 21:53:31 +0100

    Création de la classe AbstractPage : ajout de méthodes communes pour réduire la duplication de code

---
+++ pages-objects/AbstractPage.ts
@@ -0,0 +1,15 @@
+import {Page} from "@playwright/test";
+
+
+export class AbstractPage{
+    readonly page:Page
+
+
+    constructor(page:Page){
+        this.page = page
+    }
+
+    async wait(time){
+        await this.page.waitForTimeout(time)
+    }
+}
