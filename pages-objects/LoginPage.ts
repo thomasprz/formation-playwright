7d220f3191901ffe134ae4631d812b4e07c00ca3
Thomas <przegralek.thomas@gmail.com>
2025-02-24 21:55:28 +0100

    Mise à jour de la page de login pour ajouter des fonctionnalités de test visuel

--- pages-objects/LoginPage.ts
+++ pages-objects/LoginPage.ts
@@ -10,6 +10,7 @@ export class LoginPage extends AbstractPage {
     readonly submitButton: Locator
     readonly errorMessage: Locator
     readonly accountSummary : Locator
+    readonly loginForm : Locator
 
     // Init selectors using constructor
     constructor(page: Page) {
@@ -19,6 +20,7 @@ export class LoginPage extends AbstractPage {
         this.passwordInput = page.locator('#user_password')
         this.submitButton = page.locator('text=Sign in')
         this.errorMessage = page.locator('.alert-error')
+        this.loginForm = page.locator('#login_form')
     }
 
     // Define login page methods
@@ -30,5 +32,13 @@ export class LoginPage extends AbstractPage {
 
     async assertErrorMessage() {
         await expect(this.errorMessage).toContainText('Login and/or password are wrong')
+    }
+
+    async snapshotLoginForm(){
+        expect(await this.loginForm.screenshot()).toMatchSnapshot('login-form.png')
+    }
+
+    async snapshotErrorMessage(){
+        expect(await this.errorMessage.screenshot()).toMatchSnapshot('login-error.png')
     }
 }
